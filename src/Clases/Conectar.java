90c79eacdba7efc7815e13e34cb52afc5d44889c
Christian Emmanuel Delgado Castro <100442428+ChristianDelgadoCastro@users.noreply.github.com>
2023-02-22 22:00:17 -0600

    Add files via upload

---
+++ src/Clases/Conectar.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Christian Delgado
+ */
+public class Conectar {
+ 
+    Connection cn;
+    Statement st;
+    
+    public Connection conexion(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            cn=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/escuela","root","root");
+            System.out.println("Conectado");
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        return cn;
+    }
+}
